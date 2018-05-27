056780fde3a53e58a01526e272084f45991746cf
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-27 20:06:48 +0200

    Implement Hcloud SDK and refactor a little bit.

--- src/HetznerCloudResolver.php
+++ src/HetznerCloudResolver.php
@@ -2,15 +2,16 @@
 
 class HetznerCloudResolver implements \yswery\DNS\ResolverInterface
 {
-    private static $TYPE_A = 1;
-    private static $TYPE_AAAA = 28;
-    private static $API_BASE_URL = 'https://api.hetzner.cloud/v1/';
+    const TYPE_A = 1;
 
-    private $apiKey;
+    const TYPE_AAAA = 28;
+
+    private $hCloudServerEndpoint;
 
     public function __construct(string $apiKey)
     {
-        $this->apiKey = $apiKey;
+        new \LKDev\HetznerCloud\HetznerAPIClient($apiKey);
+        $this->hCloudServerEndpoint = new LKDev\HetznerCloud\Models\Servers\Servers();
     }
 
     public function getAnswer(array $query)
@@ -18,30 +19,34 @@ class HetznerCloudResolver implements \yswery\DNS\ResolverInterface
         $query[0]['qname'] = rtrim($query[0]['qname'], '.');
 
         $result = [];
+        try {
+            foreach ($this->hCloudServerEndpoint->all() as $server) {
+                if ($server->name == $query[0]['qname']) {
 
-        foreach ($this->apiCall('servers')->servers as $server) {
-            if ($server->name == $query[0]['qname']) {
+                    if ($query[0]['qtype'] == self::TYPE_A) {
+                        $result[] = $server->public_net->ipv4->ip;
+                    } else {
+                        if ($query[0]['qtype'] == self::TYPE_AAAA) {
+                            // first, use IPv6 rDNS config for name resolution
+                            foreach ($server->public_net->ipv6->dns_ptr as $address) {
+                                if ($address->dns_ptr == $query[0]['qname']) {
+                                    $result[] = $address->ip;
+                                }
+                            }
 
-                if ($query[0]['qtype'] == self::$TYPE_A) {
-                    $result[] = $server->public_net->ipv4->ip;
-                }
-
-                else if ($query[0]['qtype'] == self::$TYPE_AAAA) {
-                    // first, use IPv6 rDNS config for name resolution
-                    foreach ($server->public_net->ipv6->dns_ptr as $address) {
-                        if ($address->dns_ptr == $query[0]['qname']) {
-                            $result[] = $address->ip;
+                            // if rDNS had no result, use the default /64 block and return the first address
+                            if (empty($result)) {
+                                $result[] = str_replace('::/64', '::1', $server->public_net->ipv6->ip);
+                            }
                         }
-                    }
-
-                    // if rDNS had no result, use the default /64 block and return the first address
-                    if (empty($result)) {
-                        $result[] = str_replace('::/64', '::1', $server->public_net->ipv6->ip);
                     }
                 }
             }
-        }
+        } catch (\LKDev\HetznerCloud\APIException $exception) {
+            echo $exception->getMessage();
 
+            return 0;
+        }
         $return = [];
         foreach ($result as $address) {
             $return[] = [
@@ -50,27 +55,12 @@ class HetznerCloudResolver implements \yswery\DNS\ResolverInterface
                 'ttl' => 0,
                 'data' => [
                     'type' => $query[0]['qtype'],
-                    'value' => $address
-                ]
+                    'value' => $address,
+                ],
             ];
-
-
         }
+
         return $return;
-    }
-
-    private function apiCall($url)
-    {
-        $ch = curl_init();
-        $headers = array('Authorization: Bearer ' . $this->apiKey);
-
-        curl_setopt($ch, CURLOPT_URL, self::$API_BASE_URL . $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return json_decode($result);
     }
 
     public function allowsRecursion()
